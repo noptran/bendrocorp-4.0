e0176388678217a99c80184da68430c28e92432c
greenygh0st <dale.myszewski@gmail.com>
2020-12-07 11:13:56 -0600

    More progress

--- src/app/pages/profiles/profile-details/profile-service-record/profile-service-record.page.ts
+++ src/app/pages/profiles/profile-details/profile-service-record/profile-service-record.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { Character } from 'src/app/models/character.model';
+import { EventAttendence } from 'src/app/models/event.model';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-profile-service-record',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-service-record.page.scss'],
 })
 export class ProfileServiceRecordPage implements OnInit {
+  @Input() character: Character;
+  awardChunks = [];
 
-  constructor() { }
+  // loading indicator
+  loadingIndicator: any;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private loading: LoadingController,
+    private profileService: ProfileService
+  ) { }
+
+  chunkArray(array, size) {
+    // tslint:disable-next-line:prefer-const
+    let result = [];
+    for (const value of array) {
+      const lastArray = result[result.length - 1];
+      if (!lastArray || lastArray.length === size) {
+        result.push([value]);
+      } else {
+        lastArray.push(value);
+      }
+    }
+    return result;
+  }
+
+  attendedEvents(attends: EventAttendence[]) {
+    if (this.character) {
+      return attends.filter(x => x.attendence_type_id === 1);
+    }
+  }
+
+  async ngOnInit() {
+    if (this.router.getCurrentNavigation().extras.state) {
+      console.log('Loading route info');
+      this.character = this.router.getCurrentNavigation().extras.state.character;
+      console.log(this.character);
+      this.awardChunks = this.chunkArray(this.character.awards, 2);
+    } else {
+      // create the loading indicator
+      this.loadingIndicator = await this.loading.create({
+        message: 'Loading'
+      });
+      await this.loadingIndicator.present();
+
+
+    }
   }
 
 }
